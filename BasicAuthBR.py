d9dd13cdc558881d79e077c97f99f7b7d825d21b
guptachiku <101277170+guptachiku@users.noreply.github.com>
2022-10-11 11:52:37 +0530

    Update BasicAuthBR.py

--- BasicAuthBR.py
+++ BasicAuthBR.py
@@ -1,29 +1,17 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 desired_cap = {
-"os" : "OS X",
-"os_version" : "Big Sur",
-"browser" : "Chrome",
-"browser_version" : "latest",
-"browserstack.local" : "false",
-"browserstack.selenium_version" : "3.14.0"
+    "os_version": "14",
+    "device": "iPhone 11 Pro Max",
+    "real_mobile": "true",
+    "browserstack.appium_version": "1.21.0",
+    "unhandledPromptBehavior": "ignore",
 }
 driver = webdriver.Remote(
-    command_executor='https://chiraggupta_1Swh8i:TFyzx8ev4ztpJ6Lh4ckx@hub-cloud.browserstack.com/wd/hub',
+    command_executor='http://chiraggupta_1Swh8i:TFyzx8ev4ztpJ6Lh4ckx@hub-cloud.browserstack.com/wd/hub',
     desired_capabilities=desired_cap)
 
-driver.get("https://the-internet.herokuapp.com/login")
-driver.maximize_window    
-driver.find_element_by_id("username").send_keys("chirag");
-driver.find_element_by_id("password").send_keys("Hello");
-driver.find_element_by_xpath("//i[@class=\"fa fa-2x fa-sign-in\"]").click();
-time.sleep(3)
+driver.get("https://dev.toms.com/on/demandware.store/Sites-toms-us-Site?qaSource=auto")
+driver.execute_script('browserstack_executor: {\"action\": \"sendBasicAuth\", \"arguments\": {\"username\":\"storefront\", \"password\": \"toms2020\", \"timeout\": \"10000\"}}')
+time.sleep(10)
 driver.quit()
-
-    
